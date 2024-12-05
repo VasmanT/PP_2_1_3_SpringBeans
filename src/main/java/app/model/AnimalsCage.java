7479d0f5538124a52a6e7c54261c8a51c3c4b763
VasmanT <vasman@mail.ru>
2024-12-05 10:44:52 +0300

    Add files via upload

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,28 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    private Timer timer;//это поле класса AmimalsCage. Cпринг создал объект типа timer, указали чтобы он сюда подтянулся
+
+    public Timer getTimer() { // метод возвращает какой то объект типа Timer
+        return timer; // возвращаем поле timer класса An...Cage
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(new Timer().getTime());// вызываем метод getTime
         System.out.println("________________________");
     }
 }
